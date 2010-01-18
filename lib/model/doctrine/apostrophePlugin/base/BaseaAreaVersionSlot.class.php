e419a885466d0b637a57c6bbbbce02a9a1a60a5e
Tom Boutell <tom@punkave.com>
2010-01-18 22:44:50 +0000

    autogenerated stuff. It works
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/trunk@10 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/apostrophePlugin/base/BaseaAreaVersionSlot.class.php
+++ lib/model/doctrine/apostrophePlugin/base/BaseaAreaVersionSlot.class.php
@@ -28,7 +28,7 @@
  * @package    cmstest
  * @subpackage model
  * @author     Your name here
- * @version    SVN: $Id: Builder.php 6820 2009-11-30 17:27:49Z jwage $
+ * @version    SVN: $Id: Builder.php 7021 2010-01-12 20:39:49Z lsmith $
  */
 abstract class BaseaAreaVersionSlot extends sfDoctrineRecord
 {
