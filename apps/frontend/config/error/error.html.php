1b8977c80107bd3bc98e914cf700ca7f4cac14c6
John Benson <johnny@punkave.com>
2010-02-15 22:19:42 +0000

    updated bundled 500 error page to have a very basic empty html document
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/trunk@356 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ apps/frontend/config/error/error.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+		
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="title" content="Home" />
+	<title>500 Error</title>
+	<script type="text/javascript" src="/sfJqueryReloadedPlugin/js/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="/apostrophePlugin/js/aUI.js"></script>
+	<script type="text/javascript" src="/apostrophePlugin/js/aControls.js"></script>
+	<script type="text/javascript" src="/apostrophePlugin/js/a.js"></script>
+	<link rel="stylesheet" type="text/css" media="screen" href="/apostrophePlugin/css/a.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+</head>
+
+<body class="a-home">
+  
+<div id="a-wrapper">
+
+   <div id="a-header"><h1>500 Error.</h1></div>
+
+</div>
+
+</body>
+</html>
