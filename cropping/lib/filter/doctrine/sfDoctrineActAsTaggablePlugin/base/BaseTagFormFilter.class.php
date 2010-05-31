b9b055fe8bde72a1fce05ecc68f0a80e4ad29225
Jake Hiller <jake@punkave.com>
2010-05-31 20:00:20 +0000

    constraint/css changes
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/features@1506 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- cropping/lib/filter/doctrine/sfDoctrineActAsTaggablePlugin/base/BaseTagFormFilter.class.php
+++ cropping/lib/filter/doctrine/sfDoctrineActAsTaggablePlugin/base/BaseTagFormFilter.class.php
@@ -3,7 +3,7 @@
 /**
  * Tag filter form base class.
  *
- * @package    symfony
+ * @package    asandbox
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
--- cropping/lib/model/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardRememberKey.class.php
+++ cropping/lib/model/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardRememberKey.class.php
@@ -22,7 +22,7 @@
  * @method sfGuardRememberKey setIpAddress()    Sets the current record's "ip_address" value
  * @method sfGuardRememberKey setSfGuardUser()  Sets the current record's "sfGuardUser" value
  * 
- * @package    symfony
+ * @package    asandbox
  * @subpackage model
  * @author     Your name here
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
