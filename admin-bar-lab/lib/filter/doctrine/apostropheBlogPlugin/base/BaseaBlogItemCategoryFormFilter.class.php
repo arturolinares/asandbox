c9cfc1fbb5341b62b77c986d6ebb3e8855c374cd
Tom Boutell <tom@punkave.com>
2010-05-31 16:45:04 +0000

    admin bar lab day project sandbox
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/features@1476 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ admin-bar-lab/lib/filter/doctrine/apostropheBlogPlugin/base/BaseaBlogItemCategoryFormFilter.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * aBlogItemCategory filter form base class.
+ *
+ * @package    asandbox
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ */
+abstract class BaseaBlogItemCategoryFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+    ));
+
+    $this->setValidators(array(
+    ));
+
+    $this->widgetSchema->setNameFormat('a_blog_item_category_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'aBlogItemCategory';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'blog_item_id'     => 'Number',
+      'blog_category_id' => 'Number',
+    );
+  }
+}
