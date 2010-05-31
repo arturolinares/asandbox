a23405ab3ca496c63252a69c4a11ce6e278bc651
Tom Boutell <tom@punkave.com>
2010-05-31 16:45:44 +0000

    organizing feature branches
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/features@1477 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ simplemedia/lib/filter/doctrine/apostropheMediaCMSSlotsPlugin/aButtonSlotFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * aButtonSlot filter form.
+ *
+ * @package    filters
+ * @subpackage aButtonSlot *
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+ */
+class aButtonSlotFormFilter extends PluginaButtonSlotFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ simplemedia/lib/form/doctrine/apostropheMediaPlugin/aMediaItemForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * aMediaItem form.
+ *
+ * @package    form
+ * @subpackage aMediaItem
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class aMediaItemForm extends PluginaMediaItemForm
+{
+  public function configure()
+  {
+  }
+}
