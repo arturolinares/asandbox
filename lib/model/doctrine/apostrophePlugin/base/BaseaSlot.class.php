725d84927ee56b6cd22c56148a59bfdea1292156
Tom Boutell <tom@punkave.com>
2010-07-12 16:10:49 +0000

    Our default fixtures now create separate superadmin and admin accounts. The superadmin account can create and remove permissions and groups from the system, while the admin account can only edit users and add them to existing groups. This is currently best for client admin use. Of course you can override these things
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/1.4@1731 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/apostrophePlugin/base/BaseaSlot.class.php
+++ lib/model/doctrine/apostrophePlugin/base/BaseaSlot.class.php
@@ -62,22 +62,6 @@ abstract class BaseaSlot extends sfDoctrineRecord
              ));
 
         $this->setSubClasses(array(
-             'aBlogSlot' => 
-             array(
-              'type' => 'aBlog',
-             ),
-             'aBlogSingleSlot' => 
-             array(
-              'type' => 'aBlogSingle',
-             ),
-             'aEventSlot' => 
-             array(
-              'type' => 'aEvent',
-             ),
-             'aEventSingleSlot' => 
-             array(
-              'type' => 'aEventSingle',
-             ),
              'aTextSlot' => 
              array(
               'type' => 'aText',
@@ -121,6 +105,22 @@ abstract class BaseaSlot extends sfDoctrineRecord
              'aNewRichTextSlot' => 
              array(
               'type' => 'aNewRichText',
+             ),
+             'aBlogSlot' => 
+             array(
+              'type' => 'aBlog',
+             ),
+             'aBlogSingleSlot' => 
+             array(
+              'type' => 'aBlogSingle',
+             ),
+             'aEventSlot' => 
+             array(
+              'type' => 'aEvent',
+             ),
+             'aEventSingleSlot' => 
+             array(
+              'type' => 'aEventSingle',
              ),
              ));
     }
